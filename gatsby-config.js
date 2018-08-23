187b27d676616eec6cc9b4369724b8c4735901f1
Juha Aittamaa <>
2018-08-23 22:37:20 +0300

    Updates

--- gatsby-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'Melvin',
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
-  pathPrefix: "/melvin-site"
-}
---
+++ path----2fb0730507ccf0b40d92.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{104:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Melvin"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----2fb0730507ccf0b40d92.js.map
